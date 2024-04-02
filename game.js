4e97f5c18a2ec4cc554ce18a68bdffc5fd6e1034
29715 <2971570963@qq.com>
2024-04-02 14:40:46 +0800

    屏幕和触屏适配

--- game.js
+++ game.js
@@ -1,7 +1,12 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        parent: 'game-container',
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 800,
+        height: 600
+    },
     physics: {
         default: 'arcade',
         arcade: {
@@ -18,40 +23,36 @@ var config = {
 
 var player;
 var stars;
-var cursors;
 var score = 0;
 var scoreText;
+var touchControl;
 
 var game = new Phaser.Game(config);
 
-function preload () {
-    this.load.image('sky', 'https://labs.phaser.io/assets/skies/space3.png');
-    this.load.image('ground', 'https://labs.phaser.io/assets/sprites/platform.png');
-    this.load.image('star', 'https://labs.phaser.io/assets/sprites/star.png');
+function preload() {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
     this.load.spritesheet('dude', 
-        'https://labs.phaser.io/assets/sprites/dude.png',
+        'assets/dude.png',
         { frameWidth: 32, frameHeight: 48 }
     );
 }
 
-function create () {
+function create() {
     this.add.image(400, 300, 'sky');
+
     var platforms = this.physics.add.staticGroup();
-
     platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
 
     player = this.physics.add.sprite(100, 450, 'dude');
-
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
     this.physics.add.collider(player, platforms);
-
-    cursors = this.input.keyboard.createCursorKeys();
 
     stars = this.physics.add.group({
         key: 'star',
@@ -59,7 +60,7 @@ function create () {
         setXY: { x: 12, y: 0, stepX: 70 }
     });
 
-    stars.children.iterate(function (child) {
+    stars.children.iterate(function(child) {
         child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
     });
 
@@ -67,28 +68,27 @@ function create () {
     this.physics.add.overlap(player, stars, collectStar, null, this);
 
     scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
-}
 
-function update () {
-    if (cursors.left.isDown) {
-        player.setVelocityX(-160);
-        player.anims.play('left', true);
-    } else if (cursors.right.isDown) {
-        player.setVelocityX(160);
-        player.anims.play('right', true);
-    } else {
+    // 触屏控制
+    this.input.on('pointerdown', function (pointer) {
+        if (pointer.x < game.config.width / 2) {
+            player.setVelocityX(-160);
+        } else {
+            player.setVelocityX(160);
+        }
+    }, this);
+
+    this.input.on('pointerup', function (pointer) {
         player.setVelocityX(0);
-        player.anims.play('turn');
-    }
-
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.setVelocityY(-330);
-    }
+    }, this);
 }
 
-function collectStar (player, star) {
-    star.disableBody(true, true);
+function update() {
+    // 更新函数留空，因为我们使用触摸控制
+}
 
+function collectStar(player, star) {
+    star.disableBody(true, true);
     score += 10;
     scoreText.setText('Score: ' + score);
 }
